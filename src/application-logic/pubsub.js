4b88c0e274f930f6712a4dd43c3a0b436504edce
Abdi-Ladif Elmi <abdiladifelmi03@gmail.com>
2021-09-27 14:03:57 -0700

    Finished creating my Pubsub object

---
+++ src/application-logic/pubsub.js
@@ -0,0 +1,20 @@
+export const Pubsub = {
+    events : {},
+
+    subscribe = (eventName, eventHandler) =>{
+        this.events[eventName] = this.events[eventName] || [];
+        this.events[eventName].push(eventHandler);
+    },
+
+    publish = (eventName, eventData) => {
+        if(this.events[eventName]){
+            this.events[eventName].forEach(eventHandler => eventHandler(eventData));  
+        }
+    },
+
+    unsubscribe = (eventName, eventHandler) => {
+        if(this.events[eventName]){
+            this.events[eventName] = this.events[eventName].filter(eventName !== eventHandler);
+      }
+    }
+};

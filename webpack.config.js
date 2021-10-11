0cf0083a168ccf04a54f68d4d9f768924de65fa2
Abdi-Ladif Elmi <abdiladifelmi03@gmail.com>
2021-10-11 15:06:15 -0700

    Forgot to take off the mode:development option in my webpack.config.js file

--- webpack.config.js
+++ webpack.config.js
@@ -1,7 +1,6 @@
 const path = require('path');
 
 module.exports = {
-    mode: 'development',
     entry : "./src/index.js",
     output : {
         filename : "main.js",
@@ -15,5 +14,4 @@ module.exports = {
             }
         ]
     },
-    devtool: 'inline-source-map',
 }
